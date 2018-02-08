80be0564b9bcaa9a621966474e1ea63b3a5fe1eb
jongwhi <31754161+jongwhi@users.noreply.github.com>
2018-02-08 18:23:41 +0900

    weather api

--- js/weather.js
+++ js/weather.js
@@ -1,25 +1,26 @@
-function WeatherService(){
-    URL forecast;
-    forecast = new URL("https://api.darksky.net/forecast/0db5c91a03124d53a58af4189b7d0cd0/37.5,127.8");
-    var weather = new XMLHttpRequest();
-    var data;
-    $.ajax({
-        url: forecast,
-        type: "get",
-        success: function(){
-            
-        }
-    })
-    weather.onreadystatechange = function(){
-        if(weather.readyState === weather.DONE){
-            if(weather.status === 200 || weather.status === 201){
-                data = weather.responseText;
-            } else{
-                console.error(weather.responseText);
-            }
-        }
-    };
-    weather.open('GET', forecast, true); 
-    weather.send();
-    return data;
+var https = require("https");
+var weatherdata="";
+function printForecast(){
+    var result = forecast();
+    return result;
 }
+
+function forecast(){
+    https.get("https://api.darksky.net/forecast/02dea48a50f8772adc70d77d54675fe0/37.8267,127.4233",function(res){
+        var info = "";
+
+        res.on("data", function (chunk) {
+            info += chunk;
+        });
+        
+        res.on("end", function result(){
+            var data = JSON.parse(info);
+            var zone = data.timezone;
+            var temp = data.currently.temperature;
+            weatherdata = zone+"<br>"+temp;
+        });
+    });
+    return weatherdata;
+}
+//export this function to use that in main file
+module.exports.forecast = forecast;
